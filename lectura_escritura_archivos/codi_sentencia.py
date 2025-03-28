41aeed708498db3e263cb895145cd0f05f10f3c6
JEHYP1021 <jehypo1021@gmail.com>
2025-03-28 09:51:47 +0100

    version 1.0.3

--- lectura_escritura_archivos/codi_sentencia.py
+++ lectura_escritura_archivos/codi_sentencia.py
@@ -1,6 +1,6 @@
 '''Se debe tener especial cuidado cuando se trabaja con archivos en python.
-Hay que verificar que python apunta hacia el espacio correcto donde se encuentra el archivo con el que pretendemos trabajar.'''
-#La primera opcion que no permite hacer esto es mediante la "ruta relativa del archivo":
+Hay que verificar que python apunta hacia el espacio de trabajo correcto donde se encuentra el archivo con el que pretendemos trabajar.'''
+#La primera opcion que nos permite hacer esto es mediante la "ruta relativa del archivo":
 #con open se abre el archivo de texto en modo "lectura" por defecto
 archivo = open("lectura_escritura_archivos/sentencia.txt")
 print(archivo.readline())#imprime la linea del archivo donde se encuentre ubicado actualmente
