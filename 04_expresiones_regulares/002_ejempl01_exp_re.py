40cdfcdd04cfbf4ad8fba52a41b54e087a1bc6b0
JEHYP1021 <jehypo1021@gmail.com>
2025-04-23 11:01:22 +0200

    comando_grep

--- 04_expresiones_regulares/002_ejempl01_exp_re.py
+++ 04_expresiones_regulares/002_ejempl01_exp_re.py
@@ -4,4 +4,9 @@ import re
 log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
 regex = r"\[(\d+)\]"
 result = re.search(regex, log)
-print(result[1])
+print(result[1]) # 12345
+
+#ejemplo 2
+#El comando grep se utiliza para buscar patrones en un archivo de texto.
+#usaremnos el comando grep para buscar la palabra "ERROR" en el archivo log.txt
+#El archivo log.txt tiene el siguiente contenido:
