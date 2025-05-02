492f54657268e8e2d13fb4a22e62266f28d2b30b
JEHYP1021 <jehypo1021@gmail.com>
2025-05-02 10:12:09 +0200

    herencia y metodo super

--- 003_clase_personas.py
+++ 003_clase_personas.py
@@ -8,12 +8,17 @@ class Personas:
         return self.edad >= 18
 
 
-class Alumnos(Personas):
+class Alumnos(Personas):#La clase Alumno hereda de la clase padre Personas.
+    #La clase Alumno tiene un atributo adicional llamado curso.
     def __init__(self, nombre, edad, curso):
-        super().__init__(nombre, edad)
+        super().__init__(nombre, edad)#El metodo super() llama al constructor de la clase padre Personas.
         self.curso = curso
        
-        
+    def mostrar_informacion(self):
+        print(f'Nombre: {self.nombre}')
+        print(f'Edad: {self.edad}')
+        print(f'Curso: {self.curso}')
+        print(f'Es mayor de edad?: {self.es_mayor_de_edad()}')       
         
 
 ramiro = Personas("Ramiro", 25)
@@ -22,3 +27,9 @@ Elena = Personas("Elena", 17)
 print(ramiro.es_mayor_de_edad())  # True
 print(Elena.es_mayor_de_edad())  # False
     
+carlos = Alumnos("Carlos", 20 , "Bachiller")
+carolina = Alumnos("Carolina", 16, "ESO")
+
+print("***LISTA DE ALUMNOS***\n")
+print(carlos.mostrar_informacion())
+print(carolina.mostrar_informacion())
